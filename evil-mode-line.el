fb145d224241d5dc74c9fd62b524ce3730a57467
minorugh <minorugh@gmail.com>
2023-01-27 07:52:17 +0900

    Fix

--- evil-mode-line.el
+++ evil-mode-line.el
@@ -37,7 +37,7 @@
 	(replace  . "#575735")
 	(operator . "DarkSeaGreen4")
 	(visual   . "SteelBlue4")
-	(emacs . "#282a36"))
+	(emacs . "#8c5353"))
   "Mode line color corresponds to Evil state."
   :type '(alist :key-type symbol :value-type string)
   :group 'evil-mode-line)
